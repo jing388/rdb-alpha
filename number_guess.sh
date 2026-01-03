f119e3033a452161b460b5e15e04459b063c46ac
Gwyneth Uy <uy.gwynethfabul@gmail.com>
2026-01-03 01:16:23 -0500

    Initial commit

---
+++ number_guess.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+# Generate random number between 1 and 1000
+SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
+
+# Prompt for username
+echo "Enter your username:"
+read USERNAME
+
+# Get user info from database
+USER_INFO=$($PSQL "SELECT user_id, games_played, best_game FROM users WHERE username='$USERNAME'")
+
+if [[ -z $USER_INFO ]]
+then
+  # New user
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+  
+  # Insert new user
+  INSERT_USER=$($PSQL "INSERT INTO users(username, games_played, best_game) VALUES('$USERNAME', 0, 0)")
+  USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
+else
+  # Existing user
+  IFS='|' read USER_ID GAMES_PLAYED BEST_GAME <<< "$USER_INFO"
+  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+fi
+
+# Start guessing game
+echo "Guess the secret number between 1 and 1000:"
+NUMBER_OF_GUESSES=0
+
+while true
+do
+  read GUESS
+  
+  # Increment guess counter
+  ((NUMBER_OF_GUESSES++))
+  
+  # Check if input is an integer
+  if [[ ! $GUESS =~ ^[0-9]+$ ]]
+  then
+    echo "That is not an integer, guess again:"
+    continue
+  fi
+  
+  # Check guess against secret number
+  if [[ $GUESS -eq $SECRET_NUMBER ]]
+  then
+    echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+    break
+  elif [[ $GUESS -gt $SECRET_NUMBER ]]
+  then
+    echo "It's lower than that, guess again:"
+  else
+    echo "It's higher than that, guess again:"
+  fi
+done
+
+# Update user stats
+if [[ -z $USER_INFO ]]
+then
+  # First game for new user
+  UPDATE_USER=$($PSQL "UPDATE users SET games_played=1, best_game=$NUMBER_OF_GUESSES WHERE user_id=$USER_ID")
+else
+  # Update existing user
+  NEW_GAMES_PLAYED=$(( GAMES_PLAYED + 1 ))
+  
+  if [[ $BEST_GAME -eq 0 || $NUMBER_OF_GUESSES -lt $BEST_GAME ]]
+  then
+    # New best game
+    UPDATE_USER=$($PSQL "UPDATE users SET games_played=$NEW_GAMES_PLAYED, best_game=$NUMBER_OF_GUESSES WHERE user_id=$USER_ID")
+  else
+    # Not a new best
+    UPDATE_USER=$($PSQL "UPDATE users SET games_played=$NEW_GAMES_PLAYED WHERE user_id=$USER_ID")
+  fi
+fi
